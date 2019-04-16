e5ee40e4b039f4fa890381b2cbae2de799f8ba94
Velktri <murrayg226@gmail.com>
2019-04-16 12:52:02 -0700

    Set landing page

---
+++ src/components/Landing.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        Landing Page
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    .container {
+        margin: auto;
+    }
+</style>
